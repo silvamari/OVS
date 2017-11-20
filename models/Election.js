b356bf35b4679b148049430c142ef3ad727d8d8f
hmarvi <Letsgotocanada@>
2017-11-20 13:55:46 -0330

    Updated Models

---
+++ models/Election.js
@@ -0,0 +1,31 @@
+//require mongoose
+let mongoose = require('mongoose');
+
+//election Schema
+let electionSchema = mongoose.Schema({
+  electionName:{
+    type : String,
+    required: true
+  },
+  electionDate:{
+    type : String,
+    required: true
+  },
+  electionType:{
+    type : String,
+    required: true
+  },
+  round :{
+    type: Number,
+    default:1
+  },
+  totalVotes:{
+    type:Number,
+    default:0
+  },
+  status:{
+    type:String, default:'active'
+  }
+});
+
+module.exports = mongoose.model('Election',electionSchema);
